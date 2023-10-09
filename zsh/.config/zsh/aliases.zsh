1a32326fb466dae46d52804d450daf6c435fef90
Maksim Koterev <makoterev@gmail.com>
2023-10-09 20:47:30 +0300

    Changed nvim config

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,6 +1,6 @@
 # alias nv="nvim"
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
-alias ll='ls -lF'
-alias la='ls -lAF'
-alias l='ls -laF'
+alias ll='ls -lhF'
+alias la='ls -lhAF'
+alias l='ls -lhaF'
